c7021bf1326f524fe4be126598aa45b1bc79d626
Rybensx <anderson_arf@hotmail.es>
2026-05-24 20:18:51 -0600

    Backend inicial

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import mysql from "mysql2/promise";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const db = await mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    mensaje: "Backend funcionando 🚀",
+  });
+});
+
+app.get("/tareas", async (req, res) => {
+  const [rows] = await db.query("SELECT * FROM tareas");
+  res.json(rows);
+});
+
+app.post("/tareas", async (req, res) => {
+  const { titulo } = req.body;
+
+  await db.query(
+    "INSERT INTO tareas (titulo) VALUES (?)",
+    [titulo]
+  );
+
+  res.json({
+    mensaje: "Tarea agregada",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
